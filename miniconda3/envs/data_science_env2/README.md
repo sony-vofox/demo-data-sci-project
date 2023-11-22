828513897d4f5439ddb1cfa4af332d822960fe4b
Sony Augustine <sony.a@vofox.com>
2023-11-22 02:31:41 -0800

    First Commit

---
+++ miniconda3/envs/data_science_env2/README.md
@@ -0,0 +1,4 @@
+# My data Science Environment
+
+This ia a helper README file with information about what is in this project!
+
---
+++ miniconda3/envs/data_science_env2/test.py
@@ -0,0 +1,11 @@
+print('HELLO WORLD!')
+
+#ctrl+shift+p --> to transform the text to uppercase
+
+air
+cat
+giraffe
+mango
+
+#sort lines ascending order
+# alt + click for selecting multiple lines and editing multiple lines at the same time --> multiline select
