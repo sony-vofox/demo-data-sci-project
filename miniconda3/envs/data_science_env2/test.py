abcbf94c4bb1d1e6d3e3d2580ff8beea17de32b9
Sony Augustine <sony.a@vofox.com>
2023-11-22 03:03:39 -0800

    Add a placeholder function for mike

--- miniconda3/envs/data_science_env2/test.py
+++ miniconda3/envs/data_science_env2/test.py
@@ -2,10 +2,24 @@ print('HELLO WORLD!')
 
 #ctrl+shift+p --> to transform the text to uppercase
 
-air
-cat
-giraffe
-mango
+# air
+# cat
+# giraffe
+# mango
 
 #sort lines ascending order
 # alt + click for selecting multiple lines and editing multiple lines at the same time --> multiline select
+
+def my_function():
+    pass
+
+
+def sarah_function():
+    pass
+
+def mikes_function():
+    pass
+
+
+    
+
